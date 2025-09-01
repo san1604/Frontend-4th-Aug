ffa2c081658c27442a39aa93d7804486cdfcb3db
Sandeep Solanki <sandeepsolankimca@gmail.com>
2025-09-01 19:39:09 +0530

    javaScript variables, functions

---
+++ index.js
@@ -0,0 +1,128 @@
+// // "use strict";
+// console.log("Hello World");
+// console.info("HTai is information");
+// console.log(6);
+// console.log("Sandeep Solanki");
+// console.log(2001);
+// let length = 100
+// console.log(length);
+
+// // var height = 150
+// // console.log(height);
+
+// // var height = 200
+// // console.log(height);
+
+// // let length = 200
+// // console.log(length);
+
+// let Height = 180
+// let anotherHeight = Height
+// console.log("Height");
+// console.log(Height);
+// console.log("Height: "+Height);
+
+
+// console.log(anotherHeight);
+
+// let weight // Declaration
+// weight = 50 // Initialization
+// console.log(weight); //accessing the variable
+
+// console.log(weight);
+// weight = "100"
+// console.log(weight);
+
+
+// // count = 10
+// // console.log(count);
+
+// c = 10
+// console.log(c);
+
+
+// const name = "Aditya"
+// console.log("Constant: ", name);
+
+// // name = "Sandeep"
+// // console.log("Constant: ", name);
+
+// const age = 20
+// console.log("Age: ",age);
+
+// //programming
+// let counter;
+// console.log(counter);
+// {
+//     counter = 1
+//     {
+//         console.log(counter); // => 1
+//     }
+    
+//     let count = 2
+//     console.log("Count: ", count);
+
+//     var number = 10
+//     console.log("Number: ", number);
+    
+    
+// }
+// // count = 3
+// // console.log("Count: ", count);
+
+// console.log("Number: ", number);
+
+// counter = counter +1;
+// console.log(counter);
+
+// let height = 180
+// {
+//     let weight = 70
+//     console.log(height);
+//     console.log(weight);
+// }
+// console.log(height);
+// console.log(weight);
+
+// let height = 200;
+// {
+//    let weight = 100;
+//    {
+//       let info = "tall";
+//       console.log(height); // -> 200
+//       console.log(weight); // -> 100
+//       console.log(info); // -> tall
+//    }
+//    console.log(height); // -> 200
+//    console.log(weight); // -> 100
+//    console.log(info); // -> Uncaught ReferenceError: info is not defined
+// }
+
+var height = 180;
+{
+   var weight = 70;
+   console.log(height); // -> 180
+   console.log(weight); // -> 70  
+}
+console.log(height); // -> 180
+console.log(weight); // -> 70
+
+function helloWorldFunction(){
+    console.log("Hello");
+    console.log("World");
+}
+console.log("Let's begin")
+helloWorldFunction()
+console.log("And again");
+helloWorldFunction()
+console.log("Once more");
+helloWorldFunction()
+
+let counter = 100
+console.log("Counter: ", counter);
+{
+    let counter = 10
+    console.log("Count: ", counter);
+    
+}
+console.log("Count: ", counter);
