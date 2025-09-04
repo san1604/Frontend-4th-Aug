d34ed020c5279aa17e684940d437997c0c28897c
Sandeep Solanki <sandeepsolankimca@gmail.com>
2025-09-04 20:03:05 +0530

    String Operations

--- index.js
+++ index.js
@@ -139,22 +139,151 @@
 // console.log("Counter: ",counter);
 // var counter = 100       
 
-let name = "Aditya"
-console.log("Name", name);
-console.log("type of Name variable is: ",typeof name);
-let typeofName = typeof name
-console.log("typeofName: ", typeofName);
+// let name = "Aditya"
+// console.log("Name", name);
+// console.log("type of Name variable is: ",typeof name);
+// let typeofName = typeof name
+// console.log("typeofName: ", typeofName);
 
 
-let year = 1990
-console.log("Year: ", year);
-console.log("type of year variable is: ", typeof year);
+// let year = 1990
+// console.log("Year: ", year);
+// console.log("type of year variable is: ", typeof year);
 
-let month
-console.log("Month: ",month);
+// let month
+// console.log("Month: ",month);
 
-console.log("type of Name variable is: ", typeof month);
+// console.log("type of Name variable is: ", typeof month);
+
+// let isDataValid = true
+// let isStringTooLong = false
+// let isGameOver = false
+// continueLoop = true
+
+// console.log(false);
+// console.log(typeof false);
+// console.log(isDataValid);
+// console.log(typeof isDataValid);
+
+// const year = 1991
+// let delayInSeconds = 0.00016
+// let area = (16 * 3.14)
+// let halfArea = area / 2
+// console.log(year);
+// console.log(typeof year);
+// console.log(delayInSeconds);
+// console.log(typeof delayInSeconds);
+
+// let a =10
+// let b = 0x10
+// let c = 0o10
+// let d = 0b10
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// let x = 9e3
+// let y = 123e-5
+// console.log(x);
+// console.log(y);
+
+// let a = 1 / 0
+// let b = -Infinity
+// console.log(a);
+// console.log(b);
+// console.log(typeof a);
+// console.log(typeof b);
+// let s = "It's definitely not a number"
+// let n = s * 10
+// console.log(s);
+// console.log(n);
+// console.log(typeof n);
+
+// let big1 = 1234567890000000000000000n
+// let big2 = 1n
+// console.log(big1);
+// console.log(typeof big1);
+// console.log(big2);
+// console.log(typeof big2);
+// console.log(7n / 4n);
+
+// let big3 = 1000n + 20
+// let big4 = 1000n / 0n
+// console.log(big3);
+// console.log(big4);
 
 
+// let country = "malawi"
+// let contigent = 'africa'
+// console.log(country);
+// console.log(typeof country);
+// console.log(contigent);
+// console.log(typeof contigent);
+
+// let dialog = "He said then 'How are you' the scene is ended "
+// let dialog1 = 'He said then "How are you" the scene is ended '
+// console.log(dialog);
+// console.log(dialog1);
+
+// let message1 = 'The vessel \'mars\' called at the port'
+// let message2 = "The vessel \"mars\" called at the port"
+// console.log(message1);  // The vessel 'mars' called at the port
+// console.log(message2);  // The vessel "mars" called at the port
+// let path = "C:\\Windows"
+// console.log(path); // C:\Windows
+
+// let path = "C:\\Windows" - "Windows"
+// console.log(path);
+// let test = "100" - "10"
+// console.log(test);
+// console.log(typeof test);
+// let test1 = "100" + "10"
+// console.log(test1);
+// console.log(typeof test1);
+// let test2 = "100" * "10"
+// console.log(test2);
+// console.log(typeof test2);
+// let test3 = "100" / "10"
+// console.log(test3);
+// console.log(typeof test3);
+
+// let country = "Malawi"
+// let contigent = "Africa"
+// let sentence = `${country} is located in ${contigent}.`
+// console.log(sentence);
+
+// console.log(console)
+
+// console.time()
+// console.log("test console");
+// console.timeEnd()
+
+// let river = "Makong"
+// let character = river.charAt(2)
+// console.log(character);
+// console.log(typeof character);
+
+// let str = "My name is Aditya Gupta"
+// let str1 = "My-name-is-Aditya-Gupta"
+// console.log(str.length);
+
+// console.log("--"+str.slice(3,7)+"--"); // 3 is included and 7 is ecluded
+// console.log("--"+str.slice(3)+"--"); // 3 is included and 7 is ecluded
+// console.log(str1.split("-"));
+// console.log(str.split("a"));
+// console.log(str.split("A"));
+// console.log(str.split(" "));
+
+let declaredVar;
+console.log(typeof declaredVar); // -> undefined
+declaredVar = 5;
+console.log(typeof declaredVar); // -> number
+declaredVar = undefined;
+console.log(typeof declaredVar); // -> undefined
+// The undefined value can also be returned by the typeof operator when a non-existent variable is
+// used as an argument.
+// Console.log(typeof notDeclaredVar); // -> undefined 
+// console.log(notDeclaredVar); // -> Uncaught ReferenceError:
 
 
